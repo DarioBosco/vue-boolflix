ed49e5e14c406c12bcc744ca145874380559c360
DarioBosco <dario_bosco@protonmail.com>
2021-09-24 15:04:56 +0200

    Creazione scaffolding

---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+	<main></main>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			API_URL: 'https://api.themoviedb.org/3/movie/550?api_key=',
+			API_KEY: '87dd413a911650468c247310a01a7d52',
+		};
+	},
+};
+</script>
+
+<style></style>
