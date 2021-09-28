13e59b6674a9f02c2bfa257977684d767bd3580e
DarioBosco <dario_bosco@protonmail.com>
2021-09-28 13:46:27 +0200

    Aggiunta dati e stelle per i voti

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,15 @@
 <template>
-	<section class="movies">
-		<div>
-			<div class="titolo">Titolo: {{ movie.title ? movie.title : movie.name }}</div>
+	<div>
+		<div class="title">Titolo: {{ movie.title ? movie.title : movie.name }}</div>
+		<div class="original_title">Titolo Originale: {{ movie.original_title ? movie.original_title : movie.original_name }}</div>
+		<div class="lang">Language: <img :src="'https://www.countryflags.io/' + getFlag(movie.original_language) + '/shiny/64.png'" /></div>
+		<div class="vote">
+			Vote:
+			<i v-for="n in Math.ceil(movie.vote_average / 2)" :key="n" class="fa fa-star full"></i>
+			<i v-for="n in 5 - Math.ceil(movie.vote_average / 2)" :key="n" class="far fa-star empty"></i>
 		</div>
-	</section>
+		<hr />
+	</div>
 </template>
 
 <script>
@@ -32,5 +38,13 @@ export default {
 .poster {
 	height: 30px;
 	width: 30px;
+}
+
+.full {
+	color: red;
+}
+
+.empty {
+	color: lightgray;
 }
 </style>
